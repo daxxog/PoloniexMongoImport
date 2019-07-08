b9d8be0de36acd759a57f273639a27c23293ca61
David Volm <david@volminator.com>
2019-07-08 11:54:03 -0500

    working on date String to ObjectID logic

--- PoloniexMongoImport/Program.cs
+++ PoloniexMongoImport/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-//using System.Collections.Generic;
 using System.IO;
 using CsvHelper;
 using MongoDB.Bson;
@@ -22,10 +21,57 @@ namespace PoloniexMongoImport {
                         CsvFlowBson(header, csv, bsonRecord);
                     }
 
-                    Console.WriteLine(bsonRecord.ToString()); //todo: upsert record into db instead of printing to console
+                    Console.WriteLine(PoloniexDateStampToBytes(bsonRecord.GetValue("Date").ToString()));
+
+                    //Console.WriteLine(bsonRecord.ToString()); //todo: upsert record into db instead of printing to console
                 }
             }
         }
+
+        /* 
+         * public static byte[] PoloniexDateStampToBytes(String dateStamp)
+         * Poloniex specific boolean logic which checks for a valid String based timestamp
+         *                                 111111111
+         *                       0123456789012345678
+         * Strings in the format YYYY-MM-DD hh:mm:ss will return true
+         */
+        public static bool PoloniexDateStampValid(String dateStamp) {
+            return dateStamp.Length == 19 &&                      //check length is 19 chars, then
+                   Char.IsNumber(dateStamp, 0) &&                 //check every char in every position of the String (positions 0 - 18, total 19 chars)
+                   Char.IsNumber(dateStamp, 1) &&
+                   Char.IsNumber(dateStamp, 2) &&
+                   Char.IsNumber(dateStamp, 3) &&                 //checking for exact numbers
+                        dateStamp.Substring(4,  1).Equals("-") && //checking for exact chars
+                   Char.IsNumber(dateStamp, 5) &&
+                   Char.IsNumber(dateStamp, 6) &&
+                        dateStamp.Substring(7,  1).Equals("-") &&
+                   Char.IsNumber(dateStamp, 8) &&
+                   Char.IsNumber(dateStamp, 9) &&
+                        dateStamp.Substring(10, 1).Equals(" ") &&
+                   Char.IsNumber(dateStamp, 11) &&
+                   Char.IsNumber(dateStamp, 12) &&
+                        dateStamp.Substring(13, 1).Equals(":") &&
+                   Char.IsNumber(dateStamp, 14) &&
+                   Char.IsNumber(dateStamp, 15) &&
+                        dateStamp.Substring(16, 1).Equals(":") &&
+                   Char.IsNumber(dateStamp, 17) &&
+                   Char.IsNumber(dateStamp, 18);
+        }
+
+        /* 
+         * public static byte[] PoloniexDateStampToBytes(String dateStamp)
+         * Poloniex specific code which converts a String based timestamp to a 4 byte array which Mongodb can use for the ObjectID
+         */
+        public static byte[] PoloniexDateStampToBytes(String dateStamp) {
+            if(PoloniexDateStampValid(dateStamp)) { //check for valid dateStamp
+                //Console.WriteLine(dateStamp);
+                return new byte[] { 0x01, 0x01, 0x01, 0x01 }; //todo, logic to create byte array from information contained in the String
+            } else {
+                Console.WriteLine("found bad dateStamp: " + dateStamp);
+                return new byte[] { 0x00, 0x00, 0x00, 0x00 };
+            }
+        }
+
 
         /* 
          * CsvFlowBson(String field, CsvReader csv, BsonDocument bson)
